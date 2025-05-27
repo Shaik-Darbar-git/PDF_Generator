466433578825f605f3537d9df32be725df6efff8
Shaik Darbar <shaikdarbar2002@gmail.com>
2025-05-27 10:35:54 +0530

    Initial Commit Create a gitignore and README files Create a main.py file for the coding install FPDF and import pandas finally output.pdf file is generated

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 # Importing the FPDF method
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+pdf.set_auto_page_break(auto=False, margin=0)
+
 # Importing the pandas read csv file
 df = pd.read_csv("topics.csv")
 
@@ -24,9 +26,26 @@ for index, row in df.iterrows():
     # adding a line
     pdf.line(10, 21, 200, 21)
 
+    # Adding a footer to the page or pdf file
+    pdf.ln(262)
+
+    pdf.set_font(family="Times", style="I", size=12)
+
+    pdf.set_text_color(100, 100, 100)
+
+    pdf.cell(w=0, h=8, txt=row["Topic"], align="R")
+
     # Adding a multipage of the pdf reading from the csv file
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        pdf.ln(274)
+
+        pdf.set_font(family="Times", style="I", size=12)
+
+        pdf.set_text_color(100, 100, 100)
+
+        pdf.cell(w=0, h=8, txt=row["Topic"], align="R")
+
 
 # printing the output pdf file
 pdf.output("output.pdf")
